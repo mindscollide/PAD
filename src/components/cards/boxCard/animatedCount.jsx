9c7440b81a913a4dee65b295b8e30b4b2aab7278
Huzeifa Jahangir <huzeifajahangir@gmail.com>
2025-11-21 11:21:48 +0500

    changes

--- src/components/cards/boxCard/animatedCount.jsx
+++ src/components/cards/boxCard/animatedCount.jsx
@@ -12,38 +12,40 @@ const AnimatedCount = ({ value, className = "", style = {}, flag = false }) => {
 
   return (
     <div
-      className={styles.counterWrapper}
+      className={styles.ellipsisContainer}
       style={{
         display: "flex",
         justifyContent: flag ? "flex-end" : "flex-start",
       }}
     >
-      {digits.map((digit, i) => (
-        <div key={i} className={styles.digitWrapper}>
-          {/^\d$/.test(digit) ? (
-            <div
-              className={styles.digitRoll}
-              style={{ transform: `translateY(-${digit * 10}%)` }}
-            >
-              {Array.from({ length: 10 }, (_, n) => {
-                return (
-                  <div key={n} className={className} style={style}>
-                    {n}
-                  </div>
-                );
-              })}
-            </div>
-          ) : (
-            <span
-              key={i}
-              className={`${styles.nonDigit} ${className}`}
-              style={style}
-            >
-              {String(digit)}
-            </span>
-          )}
-        </div>
-      ))}
+      <div className={styles.counterWrapper}>
+        {digits.map((digit, i) => (
+          <div key={i} className={styles.digitWrapper}>
+            {/^\d$/.test(digit) ? (
+              <div
+                className={styles.digitRoll}
+                style={{ transform: `translateY(-${digit * 10}%)` }}
+              >
+                {Array.from({ length: 10 }, (_, n) => {
+                  return (
+                    <div key={n} className={className} style={style}>
+                      {n}
+                    </div>
+                  );
+                })}
+              </div>
+            ) : (
+              <span
+                key={i}
+                className={`${styles.nonDigit} ${className}`}
+                style={style}
+              >
+                {String(digit)}
+              </span>
+            )}
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
